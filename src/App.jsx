2296911e1c88c3e3e98aae70b7421418fde42694
spycrab <wialex121@gmail.com>
2025-10-19 23:44:37 -0400

    feat: search location component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
-import { useState, useEffect } from "react";
 import "./App.css";
+import { useState, useEffect, useCallback } from "react";
+import SearchForm from "./features/search/SearchForm";
+
+const url = "https://geocoding-api.open-meteo.com/v1/search?name=";
 
 function App() {
   const [queryString, setQueryString] = useState("");
-  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${queryString}&count=10&language=en&format=json`;
+  const [location, setLocation] = useState(null);
+
+  const encodeUrl = useCallback(() => {
+    return `${url}${encodeURIComponent(
+      queryString
+    )}&count=10&language=en&format=json`;
+  }, [queryString]);
 
   useEffect(() => {
-    const handleSearch = async () => {
+    if (!queryString) {
+      return;
+    }
+
+    const fetchLocation = async () => {
       try {
-        const response = await fetch(`${url}`);
+        const response = await fetch(encodeUrl());
         if (!response.ok) {
           throw new Error(`Response Status: ${response.status}`);
         }
-        const result = await response.json();
-        console.log(result);
+        const { results } = await response.json();
+        setLocation(results);
+        console.log(results);
       } catch (error) {
         console.log(error.message);
       }
     };
 
-    handleSearch();
-  }, [queryString]);
+    fetchLocation();
+  }, [queryString, encodeUrl]);
 
-  const preventRefresh = (e) => {
-    e.preventDefault();
-  };
   return (
     <>
-      <form onSubmit={preventRefresh}>
-        <input
-          type="text"
-          value={queryString}
-          name="search"
-          onChange={(e) => {
-            setQueryString(e.target.value);
-          }}
-        />
-        <button type="button">Search</button>
-      </form>
+      <SearchForm setQueryString={setQueryString}></SearchForm>
     </>
   );
 }
--- src/features/search/SearchForm.jsx
+++ src/features/search/SearchForm.jsx
@@ -1,8 +1,33 @@
-function SearchForm() {
+import { useState, useEffect } from "react";
+
+function SearchForm({ setQueryString }) {
+  const [localQueryString, setLocalQueryString] = useState("");
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      console.log("SearchFrom is accessed");
+      setQueryString(localQueryString);
+    }, 500);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [localQueryString, setQueryString]);
+
+  const preventRefresh = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <form>
-        <input type="text" />
+      <form onSubmit={preventRefresh}>
+        <input
+          type="text"
+          id="search"
+          value={localQueryString}
+          onChange={(e) => {
+            setLocalQueryString(e.target.value);
+          }}
+        />
         <button type="button">Search</button>
       </form>
     </>
